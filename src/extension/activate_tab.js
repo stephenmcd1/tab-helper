b247c85e6923b646e27bf8d865ebaaaada2b77cf
Stephen McDaniel <smcdaniel@gmail.com>
2023-09-05 09:53:56 -0700

    Handle incomplete URLs passed to activate-tab

--- src/extension/activate_tab.js
+++ src/extension/activate_tab.js
@@ -2,8 +2,8 @@ function activateTab(url) {
     //Look for a tab with this exact URL
     chrome.tabs.query({ url: url },
         function (res) {
-            //If one wasn't found, just open it in a new tab
-            if (res.length === 0) {
+            //If one wasn't found (or the URL was not in the correct form), just open it in a new tab
+            if (!res || res.length === 0) {
                 openNewTab(url);
             } else {
                 //Otherwise, just grab the first one (hopefully there aren't multiple matches)
