cae7914a3f8cf7291734a6f312f9c58853c708d7
Stephen McDaniel <smcdaniel@gmail.com>
2024-05-06 16:11:40 -0700

    Upgrade to Manifest v3

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,4 +1,6 @@
-﻿//Perform all the one-time setup
+﻿importScripts("new_tab.js", "activate_tab.js", "native_host.js");
+
+//Perform all the one-time setup
 connectNativeHost();
 startTrackingTargetWindow();
 setupBrowserAction();
--- src/extension/new_tab.js
+++ src/extension/new_tab.js
@@ -1,22 +1,23 @@
-var targetWindowId;
-
 //Configures the Browser Action to make the target window active
 function setupBrowserAction() {
-    chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.action.onClicked.addListener(async function(tab) {
+        let { targetWindowId } = await chrome.storage.local.get(["targetWindowId"]);
+
         //If this new window is already the target, nothing to do
         if (tab.windowId === targetWindowId) {
             return;
         }
 
         //Otherwise, remember this new target and then update all the tabs
-        targetWindowId = tab.windowId;
+        await chrome.storage.local.set({ targetWindowId: tab.windowId });
         resetAllTabs();
     });
 }
 
 //Opens the given URL in the target window
-function openNewTab(url) {
-    chrome.tabs.create({url: url, windowId: targetWindowId});
+async function openNewTab(url) {
+    let { targetWindowId } = await chrome.storage.local.get(["targetWindowId"]);
+    chrome.tabs.create({ url: url, windowId: targetWindowId });
 }
 
 //Sets up the tracking of the target window
@@ -25,34 +26,35 @@ function startTrackingTargetWindow() {
     //For now, just grab whatever happens to be the current window and use that as the default target
     //TODO: Try to maintain the target window across launches of Chrome.  This will require a way to uniquely identity windows
     //      because the windowId is not persisted across application launches.
-    findCurrentWindow(function () {
+    findCurrentWindow(function() {
         //Once we know initial target window, update all the tabs
         resetAllTabs();
     });
 
     //If the target window gets closed, look for a new window to become the target
-    chrome.windows.onRemoved.addListener(function (w) {
+    chrome.windows.onRemoved.addListener(async function(w) {
+        let { targetWindowId } = await chrome.storage.local.get(["targetWindowId"]);
         if (w === targetWindowId) {
-            targetWindowId = null;
+            await chrome.storage.local.set({ targetWindowId: null });
             findCurrentWindow();
         }
     });
 
     //When any tab-related action occurs, make sure our Browser Action is in sync
-    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
         checkTargetTab(tab);
     });
 
-    chrome.tabs.onActivated.addListener(function (activeInfo) {
+    chrome.tabs.onActivated.addListener(function(activeInfo) {
         checkTarget(activeInfo.tabId, activeInfo.windowId);
     });
 
-    chrome.tabs.onDetached.addListener(function (tabId, detachInfo) {
+    chrome.tabs.onDetached.addListener(function(tabId, detachInfo) {
         //If the tab was just removed from a window, it can't possibly be in the target window any longer
         checkTarget(tabId, -9999);
     });
 
-    chrome.tabs.onAttached.addListener(function (tabId, attachInfo) {
+    chrome.tabs.onAttached.addListener(function(tabId, attachInfo) {
         checkTarget(tabId, attachInfo.newWindowId);
     });
 
@@ -60,8 +62,8 @@ function startTrackingTargetWindow() {
 }
 
 function findCurrentWindow(cb) {
-    chrome.windows.getLastFocused({}, function (win) {
-        targetWindowId = win.id;
+    chrome.windows.getLastFocused({}, async function(win) {
+        await chrome.storage.local.set({ targetWindowId: win.id });
         if (cb) {
             cb();
         }
@@ -69,8 +71,8 @@ function findCurrentWindow(cb) {
 }
 
 function resetAllTabs() {
-    chrome.tabs.query({}, function (tabs) {
-        tabs.forEach(function (t) {
+    chrome.tabs.query({}, function(tabs) {
+        tabs.forEach(function(t) {
             checkTargetTab(t);
         });
     });
@@ -80,15 +82,13 @@ function checkTargetTab(tab) {
     checkTarget(tab.id, tab.windowId);
 }
 
-function checkTarget(tabId, windowId) {
+async function checkTarget(tabId, windowId) {
+    let { targetWindowId } = await chrome.storage.local.get(["targetWindowId"]);
     var isTarget = windowId === targetWindowId;
     var badge = isTarget ? 'Pri' : '';
     var icon = isTarget ? 'browsers-128.png' : 'browsers-gray-128.png';
     var title = isTarget ? 'Tab Controller - Primary Window' : 'Tab Controller - Not Primary Window';
-    chrome.browserAction.setBadgeText({text: badge, tabId: tabId});
-    chrome.browserAction.setIcon({path: icon, tabId: tabId});
-    chrome.browserAction.setTitle({title: title,  tabId: tabId});
+    chrome.action.setBadgeText({ text: badge, tabId: tabId });
+    chrome.action.setIcon({ path: icon, tabId: tabId });
+    chrome.action.setTitle({ title: title, tabId: tabId });
 }
-
-
-
